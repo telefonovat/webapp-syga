a8735ec73687e369770a1f36513abdb111cbc6f8
phone <37133358@cuni.cz>
2024-08-04 15:36:48 +0200

    Home view skeleton

---
+++ src/components/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Test from '../test/Test.vue';
+</script>
+
+<template>
+  <div class="home-page">
+    <Test msg="This is the home page" />
+  </div>
+</template>
+
+<style scoped></style>
