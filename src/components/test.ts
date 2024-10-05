7c69860db3d67fd979939fb4f3a394e3ec954c37
Khel Tan <kheltan.engineering@gmail.com>
2024-10-05 14:41:26 +0200

    TS and Vue config for development

---
+++ src/components/test.ts
@@ -0,0 +1,5 @@
+const a = {
+  name: "Bob",
+  age: 10,
+}
+export { a };
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path';
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src'),
+    }
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+  }
+})
