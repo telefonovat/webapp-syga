e535ad9cc1ebf8c6778caf3af84430994301f1ec
Khel Tan <kheltan.engineering@gmail.com>
2024-10-08 19:24:03 +0200

    Editor store

---
+++ src/store/editor/editorStore.ts
@@ -0,0 +1,10 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+const useEditorStore = defineStore("Editor Store", () => {
+  const code = ref("");
+
+  return {
+    code,
+  }
+});
