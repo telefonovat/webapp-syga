fef5196a0ec12023f7ede8442debba74042c2608
Khel Tan <kheltan.engineering@gmail.com>
2024-12-14 22:12:15 +0100

    Chore : Update submodule

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -16,21 +16,8 @@ function logout() {
 
 <template>
   <UserDashboard />
-  <!-- <div -->
-  <!--   v-if="isAuthenticated" -->
-  <!--   class="d-flex flex-column justify-center align-center" -->
-  <!--   style="height: 100vh" -->
-  <!-- > -->
-  <!--   <v-card> -->
-  <!--     Here lies all the user info you wanted to see. Unfortunately, we -->
-  <!--     have not implemented the views yet. -->
-  <!--   </v-card> -->
   <v-btn @click="logout()" class="mt-3">Log out</v-btn>
 
   <MockFooter />
-  <!-- </div> -->
-  <!-- <div v-else> -->
-  <!--   <v-card>Please log in first</v-card> -->
-  <!-- </div> -->
 </template>
 <style scoped></style>
