024dfc961ea65bc9189f0253d32db8eec6e02d09
Khel Tan <kheltan.engineering@gmail.com>
2025-03-01 00:33:14 +0100

    test: add frame cycling and no frames cases to animation store

--- src/store/visualizer/visualizer.spec.ts
+++ src/store/visualizer/visualizer.spec.ts
@@ -1,6 +1,8 @@
 import { describe, expect, it, vi } from 'vitest';
 import { useTicker } from './ticker';
 import { createPinia } from 'pinia';
+import { useAnimationStore_ } from './animationStore';
+import { Frame } from '@/shared-types/visualization/Frame';
 
 describe('Ticker store', () => {
   it('should not tick before tick period ends', () => {
@@ -18,5 +20,55 @@ describe('Ticker store', () => {
     expect(store.shouldDoTick(Date.now())).toBe(true);
 
     vi.useRealTimers();
+  });
+});
+
+describe('Animation Store', () => {
+  it('throws errors if frame movement is done when there are no frames', () => {
+    const store = useAnimationStore_(createPinia());
+
+    store.setFrames([]);
+
+    expect(() => store.nextFrame()).toThrowError();
+    expect(() => store.prevFrame()).toThrowError();
+  });
+
+  it('should cycle through frames correctly', () => {
+    const store = useAnimationStore_(createPinia());
+
+    const testFrames: Frame[] = [
+      {
+        lineNo: [],
+        consoleLogs: 'frame 0',
+        components: [],
+      },
+      {
+        lineNo: [],
+        consoleLogs: 'frame 1',
+        components: [],
+      },
+      {
+        lineNo: [],
+        consoleLogs: 'frame 2',
+        components: [],
+      },
+    ];
+
+    store.setFrames(testFrames);
+
+    expect(store.activeFrameNumber).toBe(0);
+    expect(store.currentFrame?.consoleLogs).toBe('frame 0');
+
+    store.nextFrame();
+    expect(store.activeFrameNumber).toBe(1);
+    expect(store.currentFrame?.consoleLogs).toBe('frame 1');
+
+    store.nextFrame();
+    expect(store.activeFrameNumber).toBe(2);
+    expect(store.currentFrame?.consoleLogs).toBe('frame 2');
+
+    store.prevFrame();
+    expect(store.activeFrameNumber).toBe(1);
+    expect(store.currentFrame?.consoleLogs).toBe('frame 1');
   });
 });
