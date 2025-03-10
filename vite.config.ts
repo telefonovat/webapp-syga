f470536278a43937d142d46a7959c5a27ce3d269
telefon <kheltan.engineering@gmail.com>
2025-03-10 12:48:41 +0100

    test: add tests for graph node

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vitest/config';
+import vue from '@vitejs/plugin-vue';
 import path from 'path';
-// https://vitejs.dev/config/
+// https://vitejs.dev/config
+/// <reference types="vitest/config" />/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, './src'),
-      "@test": path.resolve(__dirname, "./test"),
-    }
+      '@': path.resolve(__dirname, './src'),
+      '@test': path.resolve(__dirname, './test'),
+    },
   },
   server: {
     port: 8080,
-    host: "0.0.0.0",
-
+    host: '0.0.0.0',
   },
-
-
-})
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+  },
+});
