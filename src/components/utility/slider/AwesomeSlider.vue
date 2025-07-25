1ecd94cc4ce486017c93695fa7ef1f42af4a4a7c
telefon <kheltan.engineering@gmail.com>
2025-07-25 15:11:29 +0200

    chore: formatting

--- src/components/utility/slider/AwesomeSlider.vue
+++ src/components/utility/slider/AwesomeSlider.vue
@@ -1,34 +1,40 @@
 <script setup lang="ts">
-import { computed, watch } from "vue";
-import AwesomeSliderPresentationBar from "./AwesomeSliderPresentationBar.vue";
-import type { AwesomeSliderModel } from "./AwesomeSliderModel";
+  import { computed, watch } from "vue";
+  import AwesomeSliderPresentationBar from "./AwesomeSliderPresentationBar.vue";
+  import type { AwesomeSliderModel } from "./AwesomeSliderModel";
 
-interface AwesomeSliderProps {
-  color?: string;
-}
+  interface AwesomeSliderProps {
+    color?: string;
+  }
 
-const model = defineModel<AwesomeSliderModel>({ required: true });
+  const model = defineModel<AwesomeSliderModel>({ required: true });
 
-const props = withDefaults(defineProps<AwesomeSliderProps>(), {
-  color: "darkcyan",
-});
+  const props = withDefaults(defineProps<AwesomeSliderProps>(), {
+    color: "darkcyan",
+  });
 
-const positionPercentage = computed(() =>
-  Math.round(
-    ((model.value.value - model.value.min) /
-      (model.value.max - model.value.min)) *
-      100,
-  ),
-);
-watch(()=>positionPercentage.value,()=>{
-console.log(model.value.max - model.value.min)
-})
+  const positionPercentage = computed(() =>
+    Math.round(
+      ((model.value.value - model.value.min) /
+        (model.value.max - model.value.min)) *
+        100,
+    ),
+  );
+  watch(
+    () => positionPercentage.value,
+    () => {
+      console.log(model.value.max);
+      console.log(model.value.max - model.value.min);
+    },
+  );
 </script>
 
 <template>
+
   <AwesomeSliderPresentationBar
     :position-percentage="positionPercentage"
     :color="props.color"
-    :text="positionPercentage.toString()"
-  />
+    :text="positionPercentage.toString()" />
+
 </template>
+
--- src/store/editor/editorStore.ts
+++ src/store/editor/editorStore.ts
@@ -1,15 +1,15 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
+import { defineStore } from "pinia";
+import { ref } from "vue";
 
-const useEditorStore = defineStore('Editor Store', () => {
-  const code = ref('');
+const useEditorStore = defineStore("Editor Store", () => {
+  const code = ref("");
   const isInDatabase = ref<boolean>(false);
   const uuid = ref<string | null>(null);
   const title = ref<string | null>(null);
   const isStarred = ref(false);
 
   function $reset() {
-    code.value = '';
+    code.value = "";
     isInDatabase.value = false;
     uuid.value = null;
     title.value = null;
