3578bde0995226762ace7c9e205cf7727c8249d7
telefon <kheltan.engineering@gmail.com>
2025-09-12 14:22:00 +0200

    chore: remove unused dependencies

--- src/main.ts
+++ src/main.ts
@@ -6,32 +6,10 @@ import { basicSetup } from "codemirror";
 import "./style.css";
 import App from "./App.vue";
 
-import "vuetify/styles";
-import "@mdi/font/css/materialdesignicons.css";
-import { createVuetify } from "vuetify";
-import * as components from "vuetify/components";
-import * as directives from "vuetify/directives";
-import { aliases, mdi } from "vuetify/iconsets/mdi";
-
 const pinia = createPinia();
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: "dark",
-  },
-  icons: {
-    defaultSet: "mdi",
-    aliases,
-    sets: {
-      mdi,
-    },
-  },
-});
 
 createApp(App)
   .use(pinia)
   .use(VueCodeMirror, { extensions: [basicSetup] })
-  .use(vuetify)
   .use(router)
   .mount("#app");
